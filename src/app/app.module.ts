91a4a232d853c335c26544f2af3ab1b7d006dfe5
desoga <done2stay@gmail.com>
2019-09-02 00:38:14 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { NgRedux, NgReduxModule } from '@angular-redux/store';
+import { IAppState, INITIAL_STATE, rootReducer } from './store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoOverviewComponent } from './todo-overview/todo-overview.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TodoOverviewComponent, TodoListComponent],
+  imports: [BrowserModule, AppRoutingModule, NgReduxModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(ngRedux: NgRedux<IAppState>) {
+    ngRedux.configureStore(rootReducer, INITIAL_STATE);
+  }
+}
